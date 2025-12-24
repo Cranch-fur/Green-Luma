acb542214895b0ffcf74dd6fe2313229cb7f702c
Cranch <spelsp1ller@gmail.com>
2025-12-25 00:31:14 +0300

    v1.2 — Yet another code cleanup, project name has been changed from "GreenLumaGUI" to "GreenLuma-GUI". — Program is now shipped with "user32SF.dll" version of Luma instead of general one.

---
+++ GreenLuma-GUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Green Luma GUI")]
+[assembly: AssemblyDescription("An unofficial tool designed to modify the Steam client. It enables users to access games and features they have not officially purchased, such as by forging licenses or activating other users game libraries.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cranch (Кранч) The Wolf©  2026")]
+[assembly: AssemblyProduct("Green Luma GUI")]
+[assembly: AssemblyCopyright("Cranch (Кранч) The Wolf©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("06c38f8a-b0a2-452c-820d-63d8308159b7")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
